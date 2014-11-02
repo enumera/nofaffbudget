bb66047e4a40924f8dd3d2f776d4aee01053b9b9
Andrew Fyfe <enumera2000@yahoo.co.uk>
2014-11-02 09:35:30 +0000

    removed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.create(name: "Fun")
-Category.create(name: "Travel")
-Category.create(name: "Food")
+
