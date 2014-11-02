3d0edb90fe4a17a6514f1a923ae8763ebd967a46
Andrew Fyfe <enumera2000@yahoo.co.uk>
2014-11-02 09:32:18 +0000

    added categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: "Fun")
+Category.create(name: "Travel")
+Category.create(name: "Food")
